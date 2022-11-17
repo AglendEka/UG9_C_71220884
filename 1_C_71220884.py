b526e536d2d3790199c29b0eeda2ad57b8770764
AglendEka <117894471+AglendEka@users.noreply.github.com>
2022-11-17 16:27:28 +0700

    Add files via upload

---
+++ 1_C_71220884.py
@@ -0,0 +1,9 @@
+namab = "charlotte"
+namad = "Cassandra"
+matakuliah = "Logika Matematika"
+grub = "A"
+input("Masukan Nama Anda:")
+input("Masukan mata Kuliah:")
+input("Masukan grub:")
+print("Haloo!",namab,namad)
+print(namad,"Tergabung dalam kelas",matakuliah,"pada grub",grub)
---
+++ 2_C_71220884.py
@@ -0,0 +1,11 @@
+kecepatan = int(input("masukan kecepatan tempuh:"))
+waktu = int(input("masukan waktu yang diperlukan:"))
+#rumus
+jarak = kecepatan*waktu
+bensin = (jarak//10)
+biaya = 15000*bensin
+print("masukan kecepatan tempuh:")
+print("masukan waktu yang diperlukan:")
+print("Teman anda mengisi bensin sebanyak","bensin","liter")
+print("Biaya yang dikeluarkan untuk mengisi bensin adalah","Rp.","biaya")
+
